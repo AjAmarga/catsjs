f614712d47ac531e0b6611bf1299cedd0adbf6e5
AjAmarga <94827191+AjAmarga@users.noreply.github.com>
2021-12-07 12:53:31 +0800

    Add files via upload

---
+++ cats.js
@@ -0,0 +1,62 @@
+export const cats = [
+  {
+    id: 1,
+    name: 'Ginger Muffin',
+    imglink: 'https://images.unsplash.com/photo-1581898262339-10bac03c5818?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'GinMuff@catmail.com'
+  },
+  {
+    id: 2,
+    name: 'Red Velvet',
+    imglink: 'https://images.unsplash.com/photo-1583083527882-4bee9aba2eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=477&q=80',
+    email: 'ReVelv@catmail.com'
+  },
+  {
+    id: 3,
+    name: 'Flora Autumn',
+    imglink: 'https://images.unsplash.com/photo-1615454299901-de13b71ecaae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'Floaut@catmail.com'
+  },
+  {
+    id: 4,
+    name: 'Gray McNappy',
+    imglink: 'https://images.unsplash.com/photo-1600880292630-ee8a00403024?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'GraMcNap@catmail.com'
+  },
+  {
+    id: 5,
+    name: 'Spicy Fluffen ',
+    imglink: 'https://images.unsplash.com/photo-1605450648855-63f9161b7ef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'SpiFl@catmail.com'
+  },
+  {
+    id: 6,
+    name: 'Del Catto',
+    imglink: 'https://images.unsplash.com/photo-1549458452-964cbc892864?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'DelCat@catmail.com'
+  },
+  {
+    id: 7,
+    name: 'Ms. GreenEye',
+    imglink: 'https://images.unsplash.com/photo-1455970022149-a8f26b6902dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'GreenEye@catmail.com'
+  },
+  {
+    id: 8,
+    name: 'Cooler Garfield',
+    imglink: 'https://images.unsplash.com/photo-1594582768484-a1bf8a868ec9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'CoolGarf@catmail.com'
+  },
+  {
+    id: 9,
+    name: 'Druid Furbaby',
+    imglink: 'https://images.unsplash.com/photo-1560009571-bbf900a1131b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'DruFur@catmail.com'
+  },
+  {
+    id: 10,
+    name: 'Forest King',
+    imglink: 'https://images.unsplash.com/photo-1587317536055-f32e61877376?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8ODJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    email: 'ForKi@catmail.com'
+  }
+];
